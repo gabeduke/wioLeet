1e65f4735922ed154b261db37852f235528977d9
gabeduke <gabeduke@gmail.com>
2018-02-14 12:04:19 -0500

    add logging

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         'click_config',
         'flask',
         'requests',
+        'schedule',
     ],
     entry_points='''
         [console_scripts]
--- src/wioleet.py
+++ src/wioleet.py
@@ -2,9 +2,12 @@ import requests
 import json
 import click
 import click_config
+import schedule
+import time
 
 from .wio_config import config
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -36,6 +39,15 @@ def wioLeet(name):
 
 
 @cli.command()
+def daemon():
+    schedule.every(1).minutes.do(log)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+@cli.command()
 def serve():
     app.run(host='0.0.0.0', port=8080)
 
@@ -44,8 +56,21 @@ def mean(numbers):
     return float(sum(numbers)) / max(len(numbers), 1)
 
 
+@cli.command()
+@app.route('/log')
+def log():
+    d = get_data()
+
+    url = "http://influxdb-influxdb:8086/write?db=leetbase"
+    payload = ("--data-binary 'soil1,wionode=wio1 value=%s'" % d)
+    req = requests.post(url, data=payload)
+    resp_dict = json.loads(req.content.decode('utf-8'))
+
+    return str(resp_dict)
+
+
 @app.route('/sensor')
-def get_wio_sensor_data():
+def get_data():
 
     url = config.user.base_url + "v1/node"
     url += "/" + str(analog['name'])                # sensor name
@@ -55,9 +80,8 @@ def get_wio_sensor_data():
         "access_token": config.node.token
     }
 
-    click.echo(url)
     req = requests.get(url, params=payload)
-
     resp_dict = json.loads(req.content.decode('utf-8'))
     sensor_data = resp_dict['analog']
+
     return str(sensor_data)
